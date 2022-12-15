2fb0546982b322c607589cd536ad6ac76fe764d8
ParikshithNayak <59831177+ParikshithNayak@users.noreply.github.com>
2022-12-15 16:43:30 +0530

    Final

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -8,17 +8,30 @@ public class Main {
   };
 
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
-    CountsByUsage counts = new CountsByUsage();
+	  CountsByUsage counts = new CountsByUsage();
+	 for(int i=0;i<cycles.length;i++)
+	 {
+		 if(cycles[i]<310)
+			 counts.lowCount++;
+		 if(cycles[i]>309&&cycles[i]<930)
+			 counts.mediumCount++;
+		 if(cycles[i]>=930)
+			 counts.highCount++;
+	 }
+   
     return counts;
   }
 
   static void testBucketingByNumberOfCycles() {
-    System.out.println("Counting batteries by usage cycles...\n");
+    System.out.println(" Counting batteries by usage cycles...\n");
     CountsByUsage counts = countBatteriesByUsage(new int[] {100, 300, 500, 600, 900, 1000});
+    System.out.println(" Low cycles:"+counts.lowCount);
+    System.out.println("\n Medium cycles:"+counts.mediumCount);
+    System.out.println("\n High cycles:"+counts.highCount);
     assert(counts.lowCount == 2);
     assert(counts.mediumCount == 3);
     assert(counts.highCount == 1);
-    System.out.println("Done counting :)\n");
+    System.out.println("\n Done counting :)\n");
   }
 
   public static void main(String[] args) {
